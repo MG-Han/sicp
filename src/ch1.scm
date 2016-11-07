d7adb676d7c6cd93d0d07c06ceb65ee3ebb33134
myounggu.han <mghan001@gmail.com>
2016-11-08 07:34:40 +0900

    I have completed chapter 1! yay!

--- src/ch1.scm
+++ src/ch1.scm
@@ -9,7 +9,8 @@
 (define (plus1 a) (+ a 1))
 (define (identity x) x)
 
-;;; exercise 1.11
+;;; >>>>>>>>>>>>>>>>>>> exercise 1.11 >>>>>>>>>>>>>>>>>>>>>>>
+;;; 
 ;;; f(n) = {n                      | n < 3,
 ;;;         f(n-1)+2f(n-2)+3f(n-3) | n >= 3}
 (define (f11 n)
@@ -25,13 +26,15 @@
 	  ((= i 2) c)
 	  (else (f-iter-sub b c (+ (* 3 a) (* 2 b) c) (- i 1)))))
   (f-iter-sub 0 1 2 n))
+;;; <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-;;; exercise 1.12
+;;; >>>>>>>>>>>>>>>>>>> exercise 1.12 >>>>>>>>>>>>>>>>>>>>>>>
 (define (pascal row col)
   (cond ((= col 1) 1)
 	((= row col) 1)
 	(else (+ (pascal (- row 1) (- col 1))
 		 (pascal (- row 1) col)))))
+;;; <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 ;;; compute square root in newton method
 (define (sqrt x)
@@ -57,6 +60,7 @@
 	(curt-iter (improve guess))))
   (curt-iter 1.0))
 
+;;; exponential
 (define (expt b n)
   (if (= n 0)
       1
@@ -117,11 +121,11 @@
 			q
 			(- count 1)))))
 
+;;; GCD
 (define (gcd a b)
   (if (= b 0)
       a
       (gcd b (remainder a b))))
-
 (define (smallest-divisor n)
   (find-divisor n 2))
 (define (find-divisor n test-divisor)
@@ -130,6 +134,8 @@
 	(else (find-divisor n (+ test-divisor 1)))))
 (define (divides? a b)
   (= (remainder b a) 0))
+
+;;; PRIME TEST
 (define (prime? n)
   (= n (smallest-divisor n)))
 (define (expmod base exp m)
@@ -163,6 +169,7 @@
       (cond ((even? low) (search-for-primes (+ low 1) high))
 	    (else (timed-prime-test low) (search-for-primes (+ low 2) high)))))
 
+;;; procedure as an argument
 (define (sum-integers a b)
   (if (> a b)
       0
@@ -175,11 +182,18 @@
   (if (> a b)
       0
       (+ (/ 1.0 (* a (+ a 2))) (pi-sum (+ a 4) b))))
+;;; recursive version of sum
+;(define (sum term a next b)
+;  (if (> a b)
+;      0
+;      (+ (term a) (sum term (next a) next b))))
 (define (sum term a next b)
-  (if (> a b)
-      0
-      (+ (term a) (sum term (next a) next b))))
-
+  (define (iter a result)
+    (if (> a b)
+	result
+	(iter (next a) (+ result (term a)))))
+  (iter a 0))
+  
 (define (sum-cubes2 a b)
   (sum cube a plus1 b))
 (define (sum-integers2 a b)
@@ -188,6 +202,7 @@
   (define (pi-term x) (/ 1.0 (* x (+ x 2))))
   (define (pi-next x) (+ x 4))
   (sum pi-term a pi-next b))
+
 (define (integral f a b dx)
   (define (next x) (+ x dx))
   (* dx
@@ -196,6 +211,7 @@
 ;;; .24998750000000042
 ;;; => (integral cube 0 1 0.001)
 ;;; .249999875000001
+
 (define (simpson-integral f a b n)
   (define (term x k)
     (cond ((= k 0) (f x))
@@ -214,3 +230,91 @@
 ;;; 1/4
 ;;; => (simpson-integral cube 0 1 1000)
 ;;; 1/4
+
+;;; exercise 1.31.a
+;(define (product term a next b)
+;  (if (> a b)
+;      1
+;      (* (term a)
+;	 (product term (next a) next b))))
+(define (term1.31 n)
+  (define (a n)
+    (if (even? n)
+	(+ n 2.0)
+        (+ n 1.0)))
+  (define (b n)
+    (if (even? n)
+        (+ n 1.0)
+        (+ n 2.0)))
+  (/ (a n) (b n)))
+; (* (product term1.31 1 plus1 1000) 4)
+
+;;; exercise 1.31.b
+(define (product term a next b)
+  (define (iter a result)
+    (if (> a b)
+	result
+	(iter (next a) (* result (term a)))))
+  (iter a 1))
+
+;;; exercise 1.32
+;(define (accumulate combiner null-value term a next b)
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (newline)
+    (display guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+	  next
+	  (try next))))
+  (try first-guess))
+
+;;; exercise 1.35
+;;;(fixed-point (lambda (x) (+ 1 (/ 1.0 x))) 1)
+
+;;; exercise 1.36
+;;;(fixed-point (lambda (x) (/ (log 1000) (log x))) 2)
+;;;(fixed-point (lambda (x) (/ (+ x (/ (log 1000) (log x))) 2)) 2)
+
+(define (deriv f)
+  (let ((dx 0.00001))
+    (lambda (x)
+      (/ (- (f (+ x dx)) (f x))
+	 dx))))
+
+(define (newtons-transform f)
+  (lambda (x) (- x (/ (f x) ((deriv f) x)))))
+
+(define (newtons-method f guess)
+  (fixed-point (newtons-transform f) guess))
+
+(define (sqrt-newton x)
+  (newtons-method (lambda (y) (- (square y) x)) 1.0))
+
+;;; exercise 1.40
+;;;(define (cubic a b c)
+;;;  (lambda (x) (+ (cube x) (* a (square x)) (* b x) c)))
+;;;(newtons-method (cubic 1 2 3) 1)
+
+;;;exercise 1.41
+;;;(define (double f)
+;;;  (lambda (x) (f (f x))))
+
+;;;exercise 1.42
+(define (compose g f)
+  (lambda (x) (g (f x))))
+;;;exercise 1.43
+(define (repeated f count)
+  (if (= 1 count)
+      f
+      (compose f (repeated f (- count 1)))))
+;;;exercise 1.44
+(define (smooth f)
+  (let ((dx 0.1))
+    (lambda (x) (/ (+ (f (- x dx)) (f x) (f (+ x dx))) 3))))
+(define (smooth-repeated f count)
+  (repeated smooth count) f)
